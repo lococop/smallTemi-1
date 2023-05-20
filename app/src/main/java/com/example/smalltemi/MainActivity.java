e54ab2415248c7844b598ad5eb559031d51160e0
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2023-05-20 23:08:38 +0900

    Add skidjoy function
    +skidjoy +strings +skidjoy_button

--- app/src/main/java/com/example/smalltemi/MainActivity.java
+++ app/src/main/java/com/example/smalltemi/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.smalltemi;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.robotemi.sdk.Robot;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +16,23 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     DatabaseReference databaseReference = firebaseDatabase.getReference();
 
+    private Robot robot;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    public void skidJoy(View view) {
+        long t = System.currentTimeMillis();
+        long end = t + 4000;
+        while (System.currentTimeMillis() < end) {
+            robot.skidJoy(1F, 0F);
+            if(System.currentTimeMillis() == end - 2000){
+                break;
+            }
+        }
+    }
 
 }
